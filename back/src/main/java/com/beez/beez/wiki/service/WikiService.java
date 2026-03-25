8c8c96a8ffd0c469a088e3e978f7d556f06b7d16
note981202-stack <note981202@gmail.com>
2026-03-25 17:40:46 +0900

    현우_20260325_위키 백 작업
    현우_20260325_위키 백 작업

--- back/src/main/java/com/beez/beez/wiki/service/WikiService.java
+++ back/src/main/java/com/beez/beez/wiki/service/WikiService.java
@@ -1,4 +1,19 @@
 package com.beez.beez.wiki.service;
 
+import com.beez.beez.wiki.dto.WikiRequest;
+import com.beez.beez.wiki.dto.WikiVersionRequest;
+
+import java.util.List;
+
+//핵심 로직이 들어가는 파일 - 어떤 기능이 필요한지 이름만 정의 (실제 구현 코드는 impl파일에서 작성)
 public interface WikiService {
+  
+  //신규 위키 등록
+  void createWiki (WikiRequest wikiRequest, WikiVersionRequest wikiVersionRequest);
+  
+  //프로젝트별 위키 목록 조회
+  List<WikiRequest> getWikiList(String projectId);
+  
+  //위키 상세 내용 조회
+  WikiVersionRequest getWikiDetail(String wikiId);
 }
--- back/src/main/java/com/beez/beez/wiki/web/WikiController.java
+++ back/src/main/java/com/beez/beez/wiki/web/WikiController.java
@@ -1,4 +1,40 @@
 package com.beez.beez.wiki.web;
 
+import com.beez.beez.wiki.dto.WikiRequest;
+import com.beez.beez.wiki.dto.WikiVersionRequest;
+import com.beez.beez.wiki.service.WikiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//사용자의 요청 URL을 받는 파일
+@RestController
+@RequestMapping
 public class WikiController {
+
+  @Autowired
+  private WikiService wikiService;
+  
+//  // 1. 위키 생성 요청 처리
+//  @PostMapping("")
+//  public String createWiki(@RequestBody WikiRequest wikiRequest,
+//                           WikiVersionRequest wikiVersionRequest) {
+//    // 현우님이 만든 서비스의 createWiki 수행!
+//    wikiService.createWiki(wikiRequest, wikiVersionRequest);
+//    return "성공적으로 등록되었습니다.";
+//  }
+  
+  // 2. 프로젝트별 위키 목록 조회
+  @GetMapping("/wiki/list/{projectId}")
+  public List<WikiRequest> getWikiList(@PathVariable String projectId) {
+    return wikiService.getWikiList(projectId);
+  }
+  
+  // 3. 위키 상세 정보 조회
+  @GetMapping("/wiki/detail/{wikiId}")
+  public WikiVersionRequest getWikiDetail(@PathVariable String wikiId) {
+    return wikiService.getWikiDetail(wikiId);
+  }
+  
 }
