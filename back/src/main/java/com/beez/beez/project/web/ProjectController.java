d19326f4d0dca56a55092cd64fb2ea48dbdb93f3
oseongnuna <7040101a@gmail.com>
2026-03-25 17:40:45 +0900

    정화_260325_프로젝트생성_백엔드

--- back/src/main/java/com/beez/beez/project/web/ProjectController.java
+++ back/src/main/java/com/beez/beez/project/web/ProjectController.java
@@ -1,4 +1,25 @@
 package com.beez.beez.project.web;
 
+import com.beez.beez.project.dto.ProjectCreateRequest;
+import com.beez.beez.project.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/project")
 public class ProjectController {
+  
+  private final ProjectService projectService;
+  
+  @GetMapping
+  public  String test() {
+    return "test";
+  }
+  
+  //프로젝트 등록
+  @PostMapping
+  public String insertProject(@RequestBody ProjectCreateRequest dto) {
+    return projectService.insertProject(dto);
+  }
 }
